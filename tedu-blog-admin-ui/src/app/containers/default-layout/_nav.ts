57c434773f868a54f2330566a18f6baedd0a4b92
phamninh <phamninh708@gmail.com>
2024-03-15 17:35:02 +0700

    Update Angular

---
+++ tedu-blog-admin-ui/src/app/containers/default-layout/_nav.ts
@@ -0,0 +1,134 @@
+import { INavData } from '@coreui/angular';
+
+export const navItems: INavData[] = [
+  {
+    name: 'Trang chủ',
+    url: '/dashboard',
+    iconComponent: { name: 'cil-speedometer' },
+    badge: {
+      color: 'info',
+      text: 'NEW'
+    }
+  },
+  {
+    name: 'Colors',
+    url: '/theme/colors',
+    iconComponent: { name: 'cil-drop' }
+  },
+  {
+    name: 'Typography',
+    url: '/theme/typography',
+    linkProps: { fragment: 'someAnchor' },
+    iconComponent: { name: 'cil-pencil' }
+  },
+  {
+    name: 'Components',
+    title: true
+  },
+  {
+    name: 'Base',
+    url: '/base',
+    iconComponent: { name: 'cil-puzzle' },
+    children: [
+      {
+        name: 'Accordion',
+        url: '/base/accordion'
+      },
+      {
+        name: 'Breadcrumbs',
+        url: '/base/breadcrumbs'
+      },
+      {
+        name: 'Cards',
+        url: '/base/cards'
+      },
+      {
+        name: 'Carousel',
+        url: '/base/carousel'
+      },
+      {
+        name: 'Collapse',
+        url: '/base/collapse'
+      },
+      {
+        name: 'List Group',
+        url: '/base/list-group'
+      },
+      {
+        name: 'Navs & Tabs',
+        url: '/base/navs'
+      },
+      {
+        name: 'Pagination',
+        url: '/base/pagination'
+      },
+      {
+        name: 'Placeholder',
+        url: '/base/placeholder'
+      },
+      {
+        name: 'Popovers',
+        url: '/base/popovers'
+      },
+      {
+        name: 'Progress',
+        url: '/base/progress'
+      },
+      {
+        name: 'Spinners',
+        url: '/base/spinners'
+      },
+      {
+        name: 'Tables',
+        url: '/base/tables'
+      },
+      {
+        name: 'Tabs',
+        url: '/base/tabs'
+      },
+      {
+        name: 'Tooltips',
+        url: '/base/tooltips'
+      }
+    ]
+  },
+  {
+    name: 'Forms',
+    url: '/forms',
+    iconComponent: { name: 'cil-notes' },
+    children: [
+      {
+        name: 'Form Control',
+        url: '/forms/form-control'
+      },
+      {
+        name: 'Select',
+        url: '/forms/select'
+      },
+      {
+        name: 'Checks & Radios',
+        url: '/forms/checks-radios'
+      },
+      {
+        name: 'Range',
+        url: '/forms/range'
+      },
+      {
+        name: 'Input Group',
+        url: '/forms/input-group'
+      },
+      {
+        name: 'Floating Labels',
+        url: '/forms/floating-labels'
+      },
+      {
+        name: 'Layout',
+        url: '/forms/layout'
+      },
+      {
+        name: 'Validation',
+        url: '/forms/validation'
+      }
+    ]
+  }
+];
